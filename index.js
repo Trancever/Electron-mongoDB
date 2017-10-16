e7f9deaf9ccb6a9083fdebe3dc61b885205dc9d1
Dawid Urbaniak <dawid.urbaniak@dataart.com>
2017-10-16 17:04:09 +0200

    Added mongodb queries.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron')
+
 let win
 
-app.on('ready', function() {
-  win = new BrowserWindow({width: 800, height: 600});
+app.on('ready', function () {
+  win = new BrowserWindow({ width: 1270, height: 900 })
 
-  win.loadURL(`file://${__dirname}/index.html`);
-});
+  win.loadURL(`file://${__dirname}/index.html`)
+})
